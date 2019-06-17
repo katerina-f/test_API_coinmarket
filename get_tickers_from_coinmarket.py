d045ca84824284bc3c80eef979c4eadad177addb
Katerina Frolova <ekaterina.frolova40@gmail.com>
2019-06-17 18:47:23 +0300

    few changes

--- get_tickers_from_coinmarket.py
+++ get_tickers_from_coinmarket.py
@@ -7,11 +7,7 @@ def get_tickers_with_max_volume():
     start = datetime.today().timestamp()
 
     tickers = coinmarketcap.ticker(start=0, limit=10, sort='volume_24h')
-    tickers_size = sys.getsizeof(tickers)
-
-    pprint.pprint(tickers_size)
     # for ticker in tickers:
-
 
     time_spent = datetime.today().timestamp() - start
     return {'time_spent': time_spent,
